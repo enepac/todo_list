538a3c97623ffd10175bf4b46f40a57e8a8ff680
enepac <enrico.acha@gmail.com>
2024-10-04 00:37:13 -0400

    Add Form in JSX, handleAddList() Function To Add Todo Items, Display Todo List in JSX

--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -14,6 +14,18 @@ const TodoList = () => {
     }
   };
 
+    const handleAddList = (index) => {
+    if (listInputs[index] && listInputs[index].trim() !== '') {
+            const newTodos = [...todos];
+            newTodos[index].lists.push(listInputs[index]);
+            setTodos(newTodos);
+            setListInputs({ ...listInputs, [index]: '' });
+        }
+    };
+    const handleListInputChange = (index, value) => {
+        setListInputs({ ...listInputs, [index]: value });
+    };
+
   return (
     <>
       <div className="todo-container">
@@ -35,6 +47,25 @@ const TodoList = () => {
         <div className="heading_todo">
             <h3>{todo.heading}</h3> {/* Display the heading here */}
             <button className="delete-button-heading">Delete Heading </button>
+        </div>
+
+        <ul>
+             {todo.lists.map((list, listIndex) => (
+               <li key={listIndex} className='todo_inside_list'>
+                <p>{list}</p>
+               </li>
+             ))}
+           </ul>
+
+
+        <div className='add_list'>
+          <input
+            type="text"
+            className="list-input"
+            placeholder="Add List"
+            value={listInputs[index] || ''}
+            onChange={(e) => handleListInputChange(index, e.target.value)}/>
+          <button className="add-list-button" onClick={() => handleAddList(index)}>Add List</button>
         </div>
     </div>
   ))}
